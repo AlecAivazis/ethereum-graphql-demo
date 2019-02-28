3725992001c6df655f8fd78769a99e850a353099
Alec Aivazis <alec@aivazis.com>
2019-02-28 14:51:25 -0800

    added itemName attribute to auction contract

--- contracts.sol
+++ contracts.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.0;
 
 // the primary contract for our auctions
 contract Auction {
-    string public foo;
+    string public itemName;
 }
 
 // the central contract that knows the address of each auction
